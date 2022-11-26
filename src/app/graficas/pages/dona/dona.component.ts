24ead55df1bc38f6b5e00fa136579a9a175e6433
ArleyRivasG <arrivasg@unipacifico.edu.co>
2022-11-25 22:39:35 -0500

    Gráfica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent  {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others' ];
+  public doughnutChartData: MultiDataSet = [
+    // [ 350, 450, 100 ],
+    // [ 50, 150, 120 ],
+    [ 250, 130, 70, 110 ],
+  ];
 
-  ngOnInit(): void {
-  }
+ public doughnutChartType: ChartType = 'doughnut'; 
 
+ public colores: Color [] = [
+   {
+     backgroundColor: [
+       '#2AE8E5',
+       '#B468EB',
+       '#E39E29',
+       '#E86E2A',
+       '#CDE807',
+     ]
+   }
+ ]
 }
+
+  
+
+
